4348022a3b64a53236ea24d9dfa67ca1f94908d2
amalyncastro <155914599+amalyncastro@users.noreply.github.com>
2024-01-26 15:36:07 -0600

    Add files via upload

--- lab2_pr5.py
+++ lab2_pr5.py
@@ -34,7 +34,7 @@ lst = []
 for student in all_student_data:
     # if coffee contains student's favorite beverage lowercased, append their first name, last name, and favorite beverage.
     if coffee(student[INDEX_BEVERAGE].lower()):
-        lst.append(print(f"{student[INDEX_FIRST]} {student[INDEX_LAST]} ({student[INDEX_BEVERAGE]})"))
+        lst.append(f"{student[INDEX_FIRST]} {student[INDEX_LAST]} ({student[INDEX_BEVERAGE]})")
         pass
 print(lst)
 
